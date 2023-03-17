480f4e90e37604e08003fd6dff8a1dd9320941d7
Lukas Mueller <lam87@cornell.edu>
2023-03-17 13:24:09 -0400

    also set the root to $home/root

--- lib/VIGS.pm
+++ lib/VIGS.pm
@@ -45,6 +45,7 @@ print STDERR "DETERMINED HOME $home\n";
 __PACKAGE__->config(
     name => 'VIGS',
     home => $home,
+    root => $home."/root",
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header => 1, # Send X-Catalyst header
