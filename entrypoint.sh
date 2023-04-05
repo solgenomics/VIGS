345796c7ffc32b7e02b64031d5f56798634eff25
Production User <production@nicotianabase.sgn.cornell.edu>
2023-04-05 01:19:24 +0000

    redirect STDERR to log file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ mkdir /tmp/vigs
 
 export VIGS_home=/home/VIGS
 
-script/vigs_server.pl --fork -d -r -p 8088
+script/vigs_server.pl --fork -d -r -p 8088 2> /var/log/vigs.log
 
 
 tail -f /home/VIGS/vigs.conf
